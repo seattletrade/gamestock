841e3e8ab7907d383b88f232dd29abd2bc91b732
Jehyun <congmul@hotmail.com>
2021-02-09 12:08:45 -0800

    Add Live current value

--- client/src/components/ChartCompanyInfo/index.js
+++ client/src/components/ChartCompanyInfo/index.js
@@ -2,10 +2,50 @@ import React from 'react'
 import Plot from 'react-plotly.js';
 
 
-export default function ChartCompanyInfo() {
+export default function ChartCompanyInfo(props) {
+
+
+
     return (
-        <div>
-            
-        </div>
+        <Plot
+            data={[props.traceState, props.volumeState]}
+            layout={{
+                yaxis: { domain: [0.5, 1] },
+                legend: { traceorder: 'reversed' },
+                yaxis2: { domain: [0, 0.1] },
+                dragmode: 'zoom',
+                margin: {
+                    l: 0,
+                    r: 0,
+                    b: 35,
+                    t: 0
+                },
+                showlegend: false,
+                xaxis: {
+                    // TODO:get range date!!,
+                    range: ['2021-02-9 07:46', '2021-02-9 18:10'],
+                    rangeslider: {
+                        visible: false
+                    },
+                    type: 'date',
+                    tickmode: 'array',
+                    // fixedrange: true,
+                    // visible: false
+
+                },
+                yaxis: {
+                    visible: false
+                },
+                font: {
+                    family: "Raleway",
+                    color: "#fff"
+                },
+                paper_bgcolor: "#000",
+                plot_bgcolor: "#000",
+                width: 375,
+                height: 200,
+            }}
+            config={{ displayModeBar: false }}
+        />
     )
 }
--- client/src/pages/Infopage.js
+++ client/src/pages/Infopage.js
@@ -3,23 +3,62 @@ import { Row, Col } from 'react-bootstrap';
 import AnimatedNumber from "animated-number-react";
 
 import API from '../utils/API'
+import ChartCompanyInfo from '../components/ChartCompanyInfo'
 
 export default function Infopage() {
 
     const [loading, setLoading] = useState(true);
     const [ticker, setTicker] = useState();
     const [companyNameState, setCompanyNameState] = useState();
+    const [oneDayStockState, setOneDayStockState] = useState();
+    const [totalDailyStockState, setTotalDailyStockState] = useState();
 
     // Button Style state
     const btnWithOutline = "btn btn-outline-primary btn-sm pt-0 pb-0 pl-2 pr-2";
-    const [button1DState, setButton1DState] = useState(btnWithOutline);
+    const [button1DState, setButton1DState] = useState("btn btn-primary btn-sm pt-0 pb-0 pl-2 pr-2");
     const [button1WState, setButton1WState] = useState(btnWithOutline);
     const [button1MState, setButton1MState] = useState(btnWithOutline);
     const [button3MState, setButton3MState] = useState(btnWithOutline);
     const [button1YState, setButton1YState] = useState(btnWithOutline);
     const [button5YState, setButton5YState] = useState(btnWithOutline);
 
-    useEffect(() => {
+    //Graph Data
+    const [traceState, setTraceState] = useState({
+        x: ['myChartXState'],
+        close: ['myCloseState'],
+        high: ['myHighState'],
+        low: ['myLowState'],
+        open: ['myOpenState'],
+        // cutomise colors
+        // line: { color: 'white' },
+        increasing: { line: { color: 'blue' }, fillcolor: 'blue' },
+        decreasing: { line: { color: 'red' }, fillcolor: 'red' },
+
+        type: 'candlestick',
+        xaxis: 'x',
+        yaxis: 'y',
+        name: '',
+    });
+
+    const [volumeState, setVolume] = useState({
+        x: ['myChartXState'],
+        y: ['myVolumeState'],
+        yaxis: 'y2',
+        type: 'bar',
+        name: 'Volume',
+        marker: {
+            color: 'rgba(100,255,255,0.3)',
+        }
+    });
+
+    const [currentValueState, setCurrentValueState] = useState();
+    const [operatorForCurrentValue, setOperatorForCurrentValue] = useState("+");
+
+
+    // Display first Graph
+    useEffect(() => {        
+        console.log("First")
+
         // Add .env file root in client
         const API_KEY = process.env.REACT_APP_API_KEY;
         let symbol = 'MSFT';
@@ -29,8 +68,8 @@ export default function Infopage() {
 
         setCompanyNameState(companyName);
 
-        const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=10min&outputsize=full&apikey=${API_KEY}`;
-        const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`;
+        const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=15min&apikey=${API_KEY}`;
+        // const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`;
 
         let xAxis = [];
         let closeValue = [];
@@ -38,42 +77,177 @@ export default function Infopage() {
         let lowValue = [];
         let openValue = [];
         let volumeValue = [];
+
+        let currentXAxis = [];
+        let currentCloseValue = [];
+        let currentHighValue = [];
+        let currentLowValue = [];
+        let currentOpenValue = [];
+        let currentVolumeValue = [];
+
         // 86400000 sec (= 1 day )
         let aDayTomiliSec = 86400000;
         // 1 day per 1
         let days = 1;
-
         API.getStockMarketData(API_Call)
             .then((res) => {
-                // console.log(res.data);
+                console.log(res.data);
+                setOneDayStockState(res.data);
                 setLoading(false);
                 setTicker(res.data["Meta Data"]["2. Symbol"])
-                for (let date in res.data["Time Series (1min)"]) {
+                for (let date in res.data["Time Series (15min)"]) {
                     // console.log(Date.parse(date));
                     // console.log(Date.now());
-                    if (Date.parse(date) > (Date.now() - (aDayTomiliSec * days))) {
-                        xAxis.push(date);
+                    // console.log(new Date(Date.parse(date)).getDate())
+                    // console.log(new Date().getDate())
 
-                        // openValue.push(res.data["Time Series (1min)"][date]['1. open']);
-                        // highValue.push(res.data["Time Series (1min)"][date]['2. high']);
-                        // lowValue.push(res.data["Time Series (1min)"][date]['3. low']);
-                        // closeValue.push(res.data["Time Series (1min)"][date]['4. close']);
-                        // volumeValue.push(res.data["Time Series (1min)"][date]['5. volume']);
+
+                    // Get a day Data for graph later
+                    if (new Date(Date.parse(date)).getDate() === new Date().getDate() - 1) {
+
+                        // Fake Date -> Change Previous day to Today
+                        xAxis.push(new Date(Date.parse(date) + aDayTomiliSec));
+
+                        openValue.push(res.data["Time Series (15min)"][date]['1. open']);
+                        highValue.push(res.data["Time Series (15min)"][date]['2. high']);
+                        lowValue.push(res.data["Time Series (15min)"][date]['3. low']);
+                        closeValue.push(res.data["Time Series (15min)"][date]['4. close']);
+                        volumeValue.push(res.data["Time Series (15min)"][date]['5. volume']);
                     }
+
+                    // get Current data for Graph
+                    if (new Date(Date.parse(date)).getDate() === new Date().getDate() - 1 &&
+                        Date.parse(date) < (new Date() - aDayTomiliSec)) {
+                        // console.log(date);
+                        // Fake Date -> Change Previous day to Today
+                        currentXAxis.push(new Date(Date.parse(date) + aDayTomiliSec));
+
+                        currentOpenValue.push(res.data["Time Series (15min)"][date]['1. open']);
+                        currentHighValue.push(res.data["Time Series (15min)"][date]['2. high']);
+                        currentLowValue.push(res.data["Time Series (15min)"][date]['3. low']);
+                        currentCloseValue.push(res.data["Time Series (15min)"][date]['4. close']);
+                        currentVolumeValue.push(res.data["Time Series (15min)"][date]['5. volume']);
+                    }
+
                 }
 
-                console.log(xAxis);
-                // console.log(openValue);
-                // console.log(highValue);
-                // console.log(lowValue);
-                // console.log(closeValue);
-                // console.log(volumeValue);
+                xAxis.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+                xAxis.splice(45, 9); // remove 04:15:00 ~ 7:00:00
+                closeValue.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+                closeValue.splice(45, 9); // remove 04:15:00 ~ 7:00:00
+                highValue.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+                highValue.splice(45, 9); // remove 04:15:00 ~ 7:00:00
+                lowValue.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+                lowValue.splice(45, 9); // remove 04:15:00 ~ 7:00:00
+                openValue.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+                openValue.splice(45, 9); // remove 04:15:00 ~ 7:00:00
 
+                // console.log(currentXAxis);
 
+                setOneDayStockState({
+                    "Time Series (15min)": {
+                        xAxis: xAxis,
+                        close: closeValue,
+                        high: highValue,
+                        low: lowValue,
+                        open: openValue,
+                    }
+                })
+
+                //Set Graph data
+                setTraceState({
+                    x: currentXAxis,
+                    close: currentCloseValue,
+                    high: currentHighValue,
+                    low: currentLowValue,
+                    open: currentOpenValue,
+                    // cutomise colors
+                    // line: { color: 'white' },
+                    increasing: { line: { color: 'blue' }, fillcolor: 'blue' },
+                    decreasing: { line: { color: 'red' }, fillcolor: 'red' },
+
+                    type: 'candlestick',
+                    xaxis: 'x',
+                    yaxis: 'y',
+                    name: '',
+                })
+
+                setVolume({
+                    x: currentXAxis,
+                    y: currentVolumeValue,
+                    yaxis: 'y2',
+                    type: 'bar',
+                    name: 'Volume',
+                    marker: {
+                        color: 'rgba(100,255,255,0.3)',
+                    }
+                })
+                console.log(currentCloseValue);
+                setCurrentValueState(currentCloseValue[0])
+                console.log("Latest currentCloseValue");
+                console.log(currentCloseValue);
 
             }).catch(err => { console.log(err); setLoading(false); })
-
     }, [])
+
+    // Display Real-time Market data
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            console.log("IN second useEffect currentValueState");
+            console.log(currentValueState);
+            let tempCurrentValueCloseOpenDifferent = [];
+            let openValue = 0;
+            if (oneDayStockState === undefined) {
+                console.log("Pass first implement for Second useEffect")
+            } else {
+                console.log("Implement Second useEffect")
+                // console.log(oneDayStockState);
+                // console.log(oneDayStockState["Time Series (15min)"]["xAxis"]);
+                for (let index in oneDayStockState["Time Series (15min)"]["xAxis"]) {
+
+                    // Calculate Current Value
+                    if (Date.parse(oneDayStockState["Time Series (15min)"]["xAxis"][index]) > (new Date())) { // 900000 ( 15min ) to take current time value
+                        // console.log(oneDayStockState["Time Series (15min)"]["close"][index])
+                        openValue = oneDayStockState["Time Series (15min)"]["open"][index]
+                        tempCurrentValueCloseOpenDifferent.push(oneDayStockState["Time Series (15min)"]["close"][index] - oneDayStockState["Time Series (15min)"]["open"][index]);
+
+                        // console.log(index)
+                    };
+                }
+            }
+            console.log("openValue");
+            console.log(openValue);
+
+            // Live current Value
+            if (currentValueState === undefined) {
+                if (openValue === 0) {
+                    console.log("pass for first implement second useEffect")
+                } else {
+                    console.log("pass for undefined currentValueState")
+                    setCurrentValueState(openValue)
+                }
+            } else {
+                console.log("currentValueState");
+                console.log(currentValueState);
+                let openCloseDifference = tempCurrentValueCloseOpenDifferent[tempCurrentValueCloseOpenDifferent.length - 1];
+                console.log(tempCurrentValueCloseOpenDifferent[tempCurrentValueCloseOpenDifferent.length - 1] / 180.0);
+                if(operatorForCurrentValue === "+"){
+                    setCurrentValueState(parseFloat(currentValueState) + parseFloat(openCloseDifference / 180.0) + openCloseDifference)
+                    setOperatorForCurrentValue("-")
+                }else{
+                    setCurrentValueState(parseFloat(currentValueState) + parseFloat(openCloseDifference / 180.0) - openCloseDifference)
+                    setOperatorForCurrentValue("+")
+                }
+            }
+
+            // Live Current Chart 1D
+            
+
+        }, 5000)
+
+
+        return () => clearInterval(intervalId);
+    }, [currentValueState])
 
     function myFetch(e) {
         const btnWithOutline = "btn btn-outline-primary btn-sm pt-0 pb-0 pl-2 pr-2";
@@ -148,50 +322,9 @@ export default function Infopage() {
                     <div className="pl-3 pt-5 mt-5" style={{ color: "white" }}>
                         <p style={{ fontSize: "14px", marginBottom: "0" }}>{ticker}</p>
                         <h3>{companyNameState}</h3>
-                        <h3>$<AnimatedNumber value={245.55298.toFixed(2)} formatValue={n => n.toFixed(2)} /></h3>
-                        {/* <h3>${parseFloat(currentValueState).toFixed(2)} </h3> */}
+                        <h3>$<AnimatedNumber value={parseFloat(currentValueState).toFixed(2)} formatValue={n => n.toFixed(2)} /></h3>
                     </div>
-                    {/* <Plot
-                        data={[traceState, volumeState]}
-                        layout={{
-                            yaxis: { domain: [0.5, 1] },
-                            legend: { traceorder: 'reversed' },
-                            yaxis2: { domain: [0, 0.1] },
-                            dragmode: 'zoom',
-                            margin: {
-                                l: 0,
-                                r: 0,
-                                b: 30,
-                                t: 0
-                            },
-                            showlegend: false,
-                            xaxis: {
-                                // autorange: false,
-                                // rangemode: "nonnegative",
-                                range: [],
-                                rangeslider: {
-                                    visible: false
-                                },
-                                type: 'date',
-                                tickmode: 'array',
-                                // fixedrange: true,
-                                // visible: false
-
-                            },
-                            yaxis: {
-                                visible: false
-                            },
-                            font: {
-                                family: "Raleway",
-                                color: "#fff"
-                            },
-                            paper_bgcolor: "#000",
-                            plot_bgcolor: "#000",
-                            width: 375,
-                            height: 200,
-                        }}
-                        config={{ displayModeBar: false }}
-                    /> */}
+                    <ChartCompanyInfo traceState={traceState} volumeState={volumeState} />
                     <Row>
                         <Col className="ml-2 mr-0 pr-0">
                             <button onClick={myFetch} className={button1DState}>1D</button>
