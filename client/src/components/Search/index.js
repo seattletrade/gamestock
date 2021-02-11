30149f3bfbccdc8040da88ea65e02e006d928464
puakehaulani <puakehaulani@gmail.com>
2021-02-10 20:43:20 -0800

    added useState to search function, state being stored is the user input, to be added to api call to display on graph.

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Search() {
+    const [searchInput, setSearchInput] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("user entered  " + searchInput); //searchInput is the state, console log is displaying the state
+    };
+
     return (
 
-        <div className="input-group mb-3 col-sm-4">
-            <input type="text" className="form-control" placeholder="Symbol" aria-label="Search" />
+        <form className="input-group mb-3 col-sm-4" onSubmit={handleSubmit}>
+            <input type="text" className="form-control" placeholder="Symbol" aria-label="Search" onChange={e => setSearchInput(e.target.value)} />
             <div className="input-group-append" >
-                <button className="btn btn-danger" type="button">Search</button>
+                <button className="btn btn-danger" type="submit">Search</button>
             </div>
-        </div >
+        </form >
 
     )
 }
