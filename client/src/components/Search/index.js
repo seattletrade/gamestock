4e4a663f394c8ec3ae8739993c0d5a3f6b3e5293
Jehyun <congmul@hotmail.com>
2021-02-10 17:01:07 -0800

    Create ChartMain Component and put it inside Search Component

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import ChartCompanyinfoMain from '../ChartCompanyinfoMain';
 
 export default function Search() {
     return (
-
+        <>
         <div className="input-group mb-3 col-sm-4">
             <input type="text" className="form-control" placeholder="Symbol" aria-label="Search" />
             <div className="input-group-append" >
                 <button className="btn btn-danger" type="button">Search</button>
             </div>
         </div >
+
+        <ChartCompanyinfoMain />
+
+        </>
 
     )
 }
