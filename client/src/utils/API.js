2764bb7c1f763f9fa58d7ea7363cdfdca561c6c2
Jehyun <congmul@hotmail.com>
2021-02-12 22:39:16 -0800

    Add Calling API Market data from Backend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,4 +8,14 @@ export default {
     getCompanyInfoData: (companySymbol) => {
         return axios.get("/api/alphaVantage/companyInfo/" + companySymbol);
     },
+
+    // outputsize=compact ( latest 100 data) , full ( data during 20 years )
+    getDailyMarketData: (companySymbol, outputSize) => {
+        return axios.get("/api/alphaVantage/dailyMargetData/" + companySymbol + "/" + outputSize);
+    },
+
+     // interval=> 15min or 60min
+    getIntraMarketData: (companySymbol, interval) =>{
+        return axios.get("/api/alphaVantage/intraMarketData/" + companySymbol + "/" + interval);
+    }
 };
