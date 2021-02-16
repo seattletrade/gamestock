794adb008bb78e1b389149f9ccd012e6bc6703d3
puakehaulani <puakehaulani@gmail.com>
2021-02-16 11:57:45 -0800

    adding in useEffect for api call

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import API from '../../utils/API'
 
 
 export default function News() {
+    const [headline, setHeadline] = useState();
 
-    API.getNews()
-        .then(res => console.log(res))
+    //usestate setup here, to useeffect below
+    useEffect(() => {
+
+        API.getNews()
+            .then(res => {
+                console.log(res)
+                setHeadline(res.data[0].headline)
+            }
+            )
+    }, [])
 
     return (
         <div>
             News here
+            {headline}
         </div>
     )
 }
