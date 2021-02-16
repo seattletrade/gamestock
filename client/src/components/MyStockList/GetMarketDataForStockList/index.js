00a55b8a8441a2d64c1526ccccce0c6812dc3102
Jehyun <congmul@hotmail.com>
2021-02-16 11:56:51 -0800

    DataProcessing to display Stock Lists

---
+++ client/src/components/MyStockList/GetMarketDataForStockList/index.js
@@ -0,0 +1,63 @@
+export default function stockDataProcessing(stockData, currentFakeTime) {
+    // console.log(stockData);
+    // console.log(dataProcessing(stockData, currentFakeTime));
+    return dataProcessing(stockData, currentFakeTime);
+}
+
+function dataProcessing(stockData, currentFakeTime) {
+
+    let symbol = stockData["Meta Data"]["2. Symbol"];
+    let currentXAxis = [];
+    let currentOpenValue = [];
+    let openValueAt930 = 0;
+    let currentValue = 0;
+
+    // 86400000 sec (= 1 day )
+    let aDayTomiliSec = 86400000;
+
+    for (let date in stockData["Time Series (15min)"]) {
+
+        if (new Date(Date.parse(date)).getDate() === new Date(Date.parse(currentFakeTime)).getDate() - 4 ) {
+            // Fake Date -> Change Previous day to Today
+            currentXAxis.push((new Date(Date.parse(date) + aDayTomiliSec)));
+            // currentXAxis.unshift((new Date(Date.parse(date) + aDayTomiliSec)).toString().substring(4, 21));
+        }
+
+        // get Current data for Graph
+        if (new Date(Date.parse(date)).getDate() === new Date(Date.parse(currentFakeTime)).getDate() - 4  &&
+            Date.parse(date) < (Date.parse(currentFakeTime) - aDayTomiliSec)) {
+                // console.log(date.substring(12, 16))
+                if(date.substring(12, 16) === "9:30"){
+                    openValueAt930 = stockData["Time Series (15min)"][date]['1. open'];
+                }
+            currentOpenValue.push(stockData["Time Series (15min)"][date]['1. open']);
+        }
+
+    }
+
+    currentXAxis.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+    currentXAxis.splice(45, 54); // remove 04:15:00 ~ 7:00:00
+    currentOpenValue.splice(0, 5); // remove 18:30:00 ~ 20:00:00
+    currentOpenValue.splice(45, 54); // remove 04:15:00 ~ 7:00:00
+
+    currentValue = currentOpenValue[0];
+
+    // console.log(openValueAt930);
+    // console.log(currentValue);
+
+    let marker = ""
+    if(currentValue >= openValueAt930){
+        marker = "blue"
+    }else{
+        marker = "red"
+    }
+
+    const graphData = {
+        symbol: symbol,
+        x: currentXAxis,
+        y: currentOpenValue,
+        marker:{ color : marker },
+    }
+
+    return graphData;
+}
--- client/src/components/MyStockList/StockChart/index.js
+++ client/src/components/MyStockList/StockChart/index.js
@@ -4,17 +4,17 @@ import Plot from 'react-plotly.js';
 
 export default function ChartUser(props) {
     // console.log("In ChartUserInfo")
-    // console.log(props)
+    // console.log(props.company.graphData)
 
     return (
         <Plot
             data={[
                 {
-                    x: ['2021-1-25', '2021-1-26', '2021-1-27','2021-1-28','2021-1-29','2021-1-30','2021-1-31','2021-2-1','2021-2-2','2021-2-3','2021-2-4','2021-2-5', '2021-2-6', '2021-2-7', '2021-2-8', '2021-2-9', '2021-2-10', '2021-2-11', '2021-2-12', '2021-2-13', '2021-2-14', '2021-2-15'],
-                    y: [4323.58, 4123.58, 4223.58, 4153.58, 4323.58, 4223.58, 4623.58, 4723.58, 4923.58, 5323.58, 5523.58, 5523.58, 5753.58, 5593.58, 5673.58, 5723.58, 5923.28, 6023.28, 6323.28, 6423.28, 6503.28, 6513.28, 6523.28],
+                    x: props.company.graphData.x,
+                    y: props.company.graphData.y,
                     type: 'scatter',
                     mode: 'lines',
-                    marker: { color: 'red' },
+                    marker: props.company.graphData.marker,
                 },
             ]}
 
@@ -34,7 +34,7 @@ export default function ChartUser(props) {
                     },
                     type: 'date',
                     // type: props.type,
-                    visible: false,
+                    visible: true,
                     tickmode: 'array',
                     // fixedrange: true,
                     // autorange: "reversed"
