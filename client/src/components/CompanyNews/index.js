c489e03491b0569dc3b40625d78ff629b9df23b4
puakehaulani <puakehaulani@gmail.com>
2021-02-17 13:53:11 -0800

    rendering news for company to match business news

--- client/src/components/CompanyNews/index.js
+++ client/src/components/CompanyNews/index.js
@@ -8,32 +8,49 @@ export default function CompanyNews(props) {
 
     //usestate setup here, to useeffect below
     useEffect(() => {
-        // let newsArr = []
-        // let businessArr = []
+        let newsArr = []
         API.getCompanyNews(props.symbol)
             .then(res => {
-                console.log(res)
-                setCompanyNews(res)
-                // newsArr = res.data;
-                // console.log(newsArr);
-                // businessArr = newsArr.filter((item) => item.category === "business");
-                // console.log(businessArr);
-                // setApiResults(businessArr)
+                newsArr = res.data.slice(0, 15);
+                console.log(newsArr)
+                setCompanyNews(newsArr);
             }
             )
     }, [])
 
-    // function formatDate(timestamp) {
-    //     const milliseconds = timestamp * 1000
-    //     const dateObject = new Date(milliseconds)
-    //     const humanDateFormat = dateObject.toLocaleString("en-US", { year: '2-digit', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' })
-    //     return humanDateFormat
-    // }
+    function formatDate(timestamp) {
+        const milliseconds = timestamp * 1000
+        const dateObject = new Date(milliseconds)
+        const humanDateFormat = dateObject.toLocaleString("en-US", { year: '2-digit', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' })
+        return humanDateFormat
+    }
 
     return (
         <>
             <h4>Latest News</h4>
-            {companyNews ? <p>this is the news</p> : <div>No news to display</div>}
+            {companyNews ? <div className="newsList">
+                {companyNews.map(item => {
+                    return (
+                        <div key={item.id}>
+                            <Row>
+                                {item.image ? <Col className="col-sm-2"> <img style={{ width: "200px", float: "left" }} src={item.image} /> </Col> : <></>}
+                                <Col className="col-sm-10">
+                                    <a href={item.url}><h5>{item.headline}</h5></a>
+                                    <sub>{formatDate(item.datetime)}</sub>
+                                </Col>
+                                <Row>
+                                    <Col className="col-sm-8">
+                                        {item.summary ? item.summary + "..." : <div></div>}
+                                    </Col>
+                                </Row>
+                            </Row>
+                            <hr className="myHr" />
+                        </div>
+                    )
+                })
+
+                }
+            </div> : <div>No news to display</div>}
         </>
     )
 }
