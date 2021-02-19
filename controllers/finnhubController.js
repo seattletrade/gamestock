9cca8bb7392c6e919047a18708fccdf72e698fa8
Jehyun <congmul@hotmail.com>
2021-02-18 17:38:58 -0800

    Recommit for add IfSTATEMENT for dotenv

--- controllers/finnhubController.js
+++ controllers/finnhubController.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 module.exports = {
     getNews: function (req, res) {
