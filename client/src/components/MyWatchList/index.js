bef831892cd404631994ae7b239474d0cebb6013
Girma Ebssa <girma.ebssa@gmail.com>
2021-02-19 01:03:26 -0800

    defines handleClick function

--- client/src/components/MyWatchList/index.js
+++ client/src/components/MyWatchList/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 
 import UserPageAPI from '../../utils/UserPageAPI';
 import API from '../../utils/API';
@@ -10,23 +10,29 @@ import NumberComma from '../NumberComma';
 
 import StockChart from './StockChart';
 import stockDataProcessing from './GetMarketDataForWatchList';
+import { useHistory } from "react-router-dom";
+
 
 import "./style.scss";
+
+
 
 export default function MyWatchList() {
     // Take FakeCurrentTime from App.js by Context
     const currentFakeTime = useContext(FakeCurrentTimeContext);
 
     const { currentUser } = useAuth();
-    const [ myStockLists , setMyStockLists] = useState([])
+    const history = useHistory()
+    const [ myWatchLists , setMyWatchLists] = useState([])
+    
 
-    useEffect(() => {
+    function loadWatchList(){
         let WatchArr = []
         let finalData = []
         API.getAllOnWatchList(currentUser.email)
         .then(watchLists => {
-            // console.log(stockLists.data);
-            WatchArr = [...stockLists.data];
+            console.log(watchLists.data);
+            WatchArr = [...watchLists.data];
             // console.log(StockArr);
             watchLists.data.map(stock => {
                 API.getIntraMarketData(stock.symbol, "15min")
@@ -44,23 +50,64 @@ export default function MyWatchList() {
                             }
                         })[0])
                         // console.log(finalData);
-                        setMyStockLists(finalData);
+                        setMyWatchLists(finalData);
                     })
                     .catch(err => console.log(err))
             })
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err))       
+    }
+
+    function handleDelete(email, symbol){
+        API.deleteOneOnWatchList(email, symbol)
+        .then((res) => loadWatchList())
+        .catch(err => console.log(err)); 
+        history.push("/gamestock/user")       
+    }
+
+    useEffect(() => {
+        loadWatchList()
+        // let WatchArr = []
+        // let finalData = []
+        // API.getAllOnWatchList(currentUser.email)
+        // .then(watchLists => {
+        //     console.log(watchLists.data);
+        //     WatchArr = [...watchLists.data];
+        //     // console.log(StockArr);
+        //     watchLists.data.map(stock => {
+        //         API.getIntraMarketData(stock.symbol, "15min")
+        //             .then(marketData => {
+        //                 // console.log(StockArr);
+        //                 // console.log(marketData);
+        //                 //Get Current Value & Graph Data
+        //                 finalData.push(WatchArr.filter(stock => {
+        //                     let graphData = stockDataProcessing(marketData.data, currentFakeTime);
+        //                     if(stock["symbol"] === graphData.symbol){
+        //                         stock["graphData"] = graphData
+        //                         return stock
+        //                     }else{
+        //                         return false
+        //                     }
+        //                 })[0])
+        //                 // console.log(finalData);
+        //                 setMyWatchLists(finalData);
+        //             })
+        //             .catch(err => console.log(err))
+        //     })
+        // })
+        // .catch(err => console.log(err))
 
     // Put Currten Value with symbols and shares which are from Stock DB to setMyStockLists
         
     }, [])
+
     return (
         <>
-            <h4>Stocks</h4>
-            <div className="myStockLists">
-                {myStockLists.length ? (
+            <h4>Watch Lists</h4>
+            <div className="myWatchLists">
+                {myWatchLists.length ? (
 
-                myStockLists.map(company => {
+                myWatchLists.map(company => {
                     
                     return(
                     <div key={company.symbol}>
@@ -70,7 +117,7 @@ export default function MyWatchList() {
                                     {company.symbol}
                                 </Row>
                                 <Row style={{ margin: "auto", color: "grey", fontSize: "10px" }}>
-                                    {company.amount} shares
+                                    {company.companyName} 
                                 </Row>
                             </Col>
                             <Col className="text-center" style={{ margin: "auto" }}>
@@ -78,6 +125,9 @@ export default function MyWatchList() {
                             </Col>
                             <Col className="text-right" style={{ margin: "auto" }}>
                                 ${parseFloat((company["graphData"].currentValue)).toFixed(2)}
+                            </Col>
+                            <Col className="text-right" style={{ margin: "auto" }}>
+                                <Button onClick={() => handleDelete(currentUser.email, company.symbol)} className="btn btn-danger">X</Button>
                             </Col>
                         </Row>
                         <hr className="myHr" />
