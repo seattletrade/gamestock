c0fd736f588bc86512439552b01dbd3746371a68
Olga Illarionova <ola.illari@gmail.com>
2021-02-18 20:01:31 -0800

    add watchlist model delete books

---
+++ models/watchlist.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const watchlistSchema = new Schema({
+  user_email: {type: String, required:true},
+  symbol: {type: String, required:true}
+  //date: { type: Date, default: Date.now }
+  
+});
+
+const Stock = mongoose.model("Watchlist", watchlistSchema );
+
+module.exports = Stock;
